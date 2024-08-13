6c060c28bf017268ff77f88af3585459734af1e0
Altair Sossai <altairsossai@gmail.com>
2024-08-13 08:28:25 -0300

    chore: Update sshServiceConnection variable in azure-pipelines.yml

--- bash/web.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo chmod +x /home/l4d2-playstats-web/L4D2PlayStats.Web
-sudo nice -n 5 screen -d -m -S "l4d2-playstats-web" sh -c 'cd /home/l4d2-playstats-web && ./L4D2PlayStats.Web'
