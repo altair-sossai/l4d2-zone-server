ff3bce77cf22d410b07c1962ea3f9482bfd14665
Altair Sossai <altairsossai@gmail.com>
2024-11-14 09:05:57 -0300

    Randomize map selection in bootstrap script for varied gameplay experience

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+maps=("c1m1_hotel" "c2m1_highway" "c3m1_plankcountry" "c4m1_milltown_a" "c5m1_waterfront" "c6m1_riverbank" "c7m1_docks" "c8m1_apartment" "c9m1_alleys" "c10m1_caves" "c11m1_greenhouse" "c12m1_hilltop" "c13m1_alpinecreek" "c14m1_junkyard")
+map=$(shuf -n 1 -e "${maps[@]}")
+
 sudo /home/steam/l4d2/left4dead2/bash/iptables.sh
-sudo /home/steam/l4d2/left4dead2/bash/start.sh 27015 c2m1_highway
+sudo /home/steam/l4d2/left4dead2/bash/start.sh 27015 $map
