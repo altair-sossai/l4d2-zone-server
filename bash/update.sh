c735652bfe70beb06a30e571bcadee6f7ce825ad
Altair Sossai <altairsossai@gmail.com>
2025-07-04 10:03:31 -0300

    Add missing update and upgrade commands to update.sh

--- bash/update.sh
+++ bash/update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get upgrade -y
+
 sudo /home/steam/l4d2/left4dead2/bash/stop.sh 27015
 sudo /home/steam/steamcmd.sh +force_install_dir /home/steam/l4d2/ +login anonymous +app_update 222860 validate +exit
 sudo /home/steam/l4d2/left4dead2/bash/bootstrap.sh
