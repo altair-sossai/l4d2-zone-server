56c0451c6de65670e36f5331a60d6763dd29f952
Altair Sossai <altairsossai@gmail.com>
2025-12-05 10:33:10 -0300

    Add timezone setting to bootstrap script

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo timedatectl set-timezone America/Sao_Paulo
+
 sudo /home/steam/l4d2/left4dead2/bash/iptables.sh
 sudo /home/steam/l4d2/left4dead2/bash/start.sh 27015 c2m1_highway
