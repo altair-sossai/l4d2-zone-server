0fe6e2ce8564952223fc893e56fe367f9ad3ef7a
Altair Sossai <altairsossai@gmail.com>
2026-05-17 20:55:51 -0300

    revert

--- ps/compile-custom-plugins.ps1
+++ ps/compile-custom-plugins.ps1
@@ -13,4 +13,3 @@ spcomp ..\addons\sourcemod\scripting\l4d2_jockey_no_deadstops.sp -o ..\addons\so
 spcomp ..\addons\sourcemod\scripting\l4d2_gameinfo_sync.sp -o ..\addons\sourcemod\plugins\l4d2_gameinfo_sync.smx
 spcomp ..\addons\sourcemod\scripting\l4d2_changelevel.sp -o ..\addons\sourcemod\plugins\fixes\l4d2_changelevel.smx
 spcomp ..\addons\sourcemod\scripting\l4d2_admin_hp.sp -o ..\addons\sourcemod\plugins\optional\l4d2_admin_hp.smx
-spcomp ..\addons\sourcemod\scripting\l4d_fix_saferoom_ghostspawn.sp -o ..\addons\sourcemod\plugins\optional\l4d_fix_saferoom_ghostspawn.smx
